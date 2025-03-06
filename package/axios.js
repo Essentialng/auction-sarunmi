af00e65cc26a507da4f57bd514b9bbcf1d3c588f
olateju <sirolateju2022@gmail.com>
2025-03-06 12:54:00 +0100

    hero section and and home content rectified

--- package/axios.js
+++ package/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: " http://localhost:3000/api/",
+    baseURL: "https://essential-auction.vercel.app/api/",
     headers: {
         "Content-Type": "application/json",
     }
